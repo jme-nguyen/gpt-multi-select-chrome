e5678ad09c1d0483e6d2de42039755f855afbe3a
jme-nguyen <104990720+jme-nguyen@users.noreply.github.com>
2024-06-17 01:32:24 +1000

    created function to parse string into question and answer (defined by two styles/templates of mcq questions)

--- server.js
+++ server.js
@@ -18,9 +18,34 @@ app.use(express.json());
 
 app.post('/multi', (req, res) => {
     const data = req.body.question;
-    console.log("Received data: ", data);
+    const parsedata = parseMCQ(data);
 
     // Respond to the request
-    resjson = JSON.stringify("Success!");
+    resjson = JSON.stringify(parsedata);
     res.status(200).send(resjson);
 })
+
+function parseMCQ(input) {
+    // Regular expressions for each style
+    const style1 = /^(.*?)\nGroup of answer choices\n\n(.+?)$/s;
+    const style2 = /^(.*?)\n\nQuestion \d+Select one:\n\n(.+?)$/s;
+  
+    let question, answers;
+  
+    if (style1.test(input)) {
+        const matches = input.match(style1);
+        if (matches) {
+            question = matches[1].trim();
+            answers = matches[2].split('\n\n').map(answer => answer.trim());
+        }
+    } else if (style2.test(input)) {
+        const matches = input.match(style2);
+        if (matches) {
+            question = matches[1].trim();
+            answers = matches[2].split('\n\n').map(answer => answer.replace(/[a-z]\.\n/, '').trim());
+        }
+    }
+  
+    console.log(question, answers);
+    return { question, answers };
+  }
